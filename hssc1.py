b8f1e93a783d307eb2e607d11e8d524ad66031ca
Zulqarnain-G <zulqarnaingujjar537@gmail.com>
2025-03-28 17:00:04 +0500

    hssc1

---
+++ hssc1.py
@@ -0,0 +1,9 @@
+num=eval (input("Enter a number:"))
+if(num==0):
+    print("Emtered number is zero")
+elif(num%2==1):
+    print("Entered number is odd")
+else:
+    print("Entered number is even")
+# Output:
+# Enter a number: 5 
